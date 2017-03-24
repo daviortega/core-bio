74bf5c743d1d2b58d252c8ed61c81391c5b55b5b
Davi Ortega <ortegad@caltech.edu>
2017-03-23 17:23:39 -0700

    trying index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let exports = module.exports = {
+	version: '0.0.4'
+}
+
+exports.fastaStream = require('./src/fasta-stream')
+exports.FastaSeq = require('./src/FastaSeq')
+exports.Seq = require('./src/Seqm')
